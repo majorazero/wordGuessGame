ba239512ed2a8529bbcad4365f2a9e2339bce931
majorazero <daniel.cw.hsu@gmail.com>
2018-08-21 23:38:25 -0700

    made hangman guesses show up and added restriction to prevent non-letter inputs to affect the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,12 +29,26 @@ function arrayToText(arr) {
     return "No Guesses Yet!";
   }
   let hText = "";
-  for (let i =0; i < arr.length; i++){
+  for(let i =0; i < arr.length; i++){
     if (i === arr.length-1){//if its the LAST element of the array
       hText += arr[i]; //its so I don't include a comma space for the last entry
     }
     else{
       hText += arr[i]+", ";
+    }
+  }
+  return hText;
+}
+
+//Second array function for hangman array display
+function gameArrayToText(arr){
+  let hText = "";
+  for(let i=0; i <arr.length; i++){
+    if(arr[i] === undefined){//if its an empty array.
+      hText += " _ ";
+    }
+    else{
+      hText += arr[i];
     }
   }
   return hText;
@@ -51,34 +65,36 @@ function randomWordOutput(){
 }
 //we'll push a word out of the bank
 let gameWord = randomWordOutput();
-let gameArray = [];
+let gameArray = new Array(gameWord.length);
 console.log(gameWord);
 //game basically starts when user tries to type in inputs
 document.onkeyup = function(event){
-  console.log(gameWord);
-  let pos = gameWord.indexOf(event.key); //position being checked
-  if(pos === -1){ //if the input is NOT part of the word, position will return -1
-    guessAttempts--; //guessAttempt gets deducted by one
-    lettersGuessed.push(event.key); //letters guessed gets pushed into the list
-  }
-  else { //only other possibility is that the letter DOES exist
-    gameArray[pos] = event.key;
-    while(pos != -1){ //run this until pos === -1, basically meanning it ran out of indexes to search for
-      console.log(pos);
-      pos = gameWord.indexOf(event.key,pos+1); //we'll look for another position of the word beyond the first place we found it
-      if (pos != -1){ //if it found something again
-        gameArray[pos] = event.key; //it will fill in the next blank space
-      }
-      //else nothing happens so we don't need to write the code, and if it ends of equaling -1, the while loop will end.
+  if (event.keyCode >= 65 && event.keyCode <= 90) { //ensures that it will only accept letters as inputs. (NO MORE META OR SHIFT YEE)
+    console.log(gameWord);
+    let pos = gameWord.indexOf(event.key); //position being checked
+    if(pos === -1){ //if the input is NOT part of the word, position will return -1
+      guessAttempts--; //guessAttempt gets deducted by one
+      lettersGuessed.push(event.key); //letters guessed gets pushed into the list
     }
-  }
+    else { //only other possibility is that the letter DOES exist
+      gameArray[pos] = event.key;
+      while(pos != -1){ //run this until pos === -1, basically meanning it ran out of indexes to search for
+        console.log(gameArray);
+        pos = gameWord.indexOf(event.key,pos+1); //we'll look for another position of the word beyond the first place we found it
+        if (pos != -1){ //if it found something again
+          gameArray[pos] = event.key; //it will fill in the next blank space
+        }
+        //else nothing happens so we don't need to write the code, and if it ends of equaling -1, the while loop will end.
+      }
+    }
 
-  //game should update document ONLY when all the data has been run through, so we put all that stuff at the end.
-  document.getElementById("wins").textContent = winCount;
-  document.getElementById("guessRemain").textContent = guessAttempts;
-  document.getElementById("lettersGuessed").textContent = arrayToText(lettersGuessed);
-
-  if(guessAttempts === 0 ){ //you lose if guessAttempts reaches 0
-    document.getElementById("loss").textContent = "YOU LOSE!";
+    //game should update document ONLY when all the data has been run through, so we put all that stuff at the end.
+    document.getElementById("wins").textContent = winCount;
+    document.getElementById("guessRemain").textContent = guessAttempts;
+    document.getElementById("lettersGuessed").textContent = arrayToText(lettersGuessed);
+    document.getElementById("word").textContent = gameArrayToText(gameArray);
+    if(guessAttempts === 0 ){ //you lose if guessAttempts reaches 0
+      document.getElementById("loss").textContent = "YOU LOSE!";
+    }
   }
 };
